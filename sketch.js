d3f6fcb513f9665bf2ae52c751209e0de5ae94a1
hcps-bothras <75810609+hcps-bothras@users.noreply.github.com>
2021-03-14 10:37:33 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,28 +3,27 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-var drops;
-var maxDrops = 100;
+var drops=[];
+var maxDrops = 50;
 
 function setup(){
    var canvas = createCanvas(700,700);
     engine = Engine.create();
     world = engine.world;
 
-    for (var i =0; i<maxDrops; i++){
-        drops.push(new Drops(random(0,600)),random(0,600));
-    } 
-
-    drops = new Drops (random(0,500),random(0,500), 10)
-    
-    
-
-    
+    if(frameCount % 150 === 0){
+        for(var i=0; i<maxDrops; i++){
+            drops.push(new Drops(random(0,400), random(0,400)));
+        }
+    }
 }
 
 function draw(){
+    Engine.update(engine);
     background (0)
     
-    drops.display(); 
+    for(var i = 0; i<maxDrops; i++){
+        drops[i].display();
+        drops[i].updateY()   
+    } 
 }   
-
